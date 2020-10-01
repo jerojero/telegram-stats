75f7b2f162875ed1e7e98d5dd4579ac62a6f293c
Michael Bartlett <bartlettmic@gmail.com>
2020-09-30 21:26:41 -0600

    top 10 words and counts

--- generate_diagrams.py
+++ generate_diagrams.py
@@ -10,14 +10,18 @@ except:
         data=json.load(f)
         
 words_per_day={}
+word_counts={}
     
 for d in data:
     words_length = len(d['text'])
     datestamp = d['date'].split("T")[0]
+    words_per_day[datestamp] = words_per_day.get(datestamp, 0) + words_length
+    
     try:
-        words_per_day[datestamp] += words_length
+        for word in d['text'].split():
+            word_counts[word] = word_counts.get(word, 0) + 1
     except:
-        words_per_day[datestamp] = words_length
+        pass
     
     
 avg_words_per_day=0
@@ -27,3 +31,4 @@ for day, word_length in words_per_day.items():
 avg_words_per_day /= len(words_per_day)
 
 print(f"Average words per day: {avg_words_per_day}")
+print(f"Top 10 occurrences of words: { { w:word_counts[w] for w in list(reversed(sorted(word_counts, key=word_counts.__getitem__)))[:10] } }")
